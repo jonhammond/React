44ef38e146ee7bce206509fb3f1b0f8a59dd3e13
Jon Hammond <jonh1016@gmail.com>
2016-07-19 18:28:32 -0600

    Finished gulpfile boilerplate

--- thumbnail-gulp/gulpfile.js
+++ thumbnail-gulp/gulpfile.js
@@ -7,7 +7,29 @@ var reactify = require('reactify'); // Convert jsx into JS
 
 gulp.task('default', function() {
   // Create a bundler to run browserify on code
+  // Watchify will watch for changes, and then re-run the gulp file
   var bundler = watchify(browserify({
-    entries: 
-  }))
-})
+    // Go to most parent component of application
+    entries: ['./src/app.jsx'],
+    // While browserify is finding dependencies, use reactify to convert jsx->JS
+    transform: [reactify],
+    // Look at these specific files during build process
+    extensions: ['.jsx'],
+    debug: true,
+    cache: {},
+    packagecache: {},
+    fullPaths: true
+  }));
+
+  function build(file) {
+    if (file) gutil.log('Recompiling ' + file);
+    return bundler
+      .bundle()
+      // Throw an error to CLI
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('./'))
+  };
+  build();
+  bundler.on('update', build)
+});
